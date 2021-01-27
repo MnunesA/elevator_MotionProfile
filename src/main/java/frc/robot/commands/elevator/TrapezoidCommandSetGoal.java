a34f3909401ade5dabea0ae963bd5f6ced1fe45b
MnunesA <matheusna100@gmail.com>
2021-01-27 00:43:26 -0300

    Versão 2.0 - Intermediate Motion Profiling

---
+++ src/main/java/frc/robot/commands/elevator/TrapezoidCommandSetGoal.java
@@ -0,0 +1,72 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.constants.ConstantsElevator;
+import frc.robot.subsystems.TrapezoidElevatorSubsystem;
+
+public class TrapezoidCommandSetGoal extends CommandBase {
+  
+  private TrapezoidElevatorSubsystem tes;
+  private TrapezoidProfile.State goal;
+  private boolean limitSwitchStatus_UP;
+  private boolean limitSwitchStatus_DOWN;
+  private double position;
+  private double velocity;
+  private double currentPosition;
+  private double currentVelocity;
+
+  public TrapezoidCommandSetGoal(TrapezoidElevatorSubsystem trapezoidProfileSubsystem, double position, double velocity) {
+    tes = trapezoidProfileSubsystem;
+    this.position = position;
+    this.velocity = velocity;
+    addRequirements(tes);
+  }
+
+  public TrapezoidCommandSetGoal(TrapezoidElevatorSubsystem trapezoidProfileSubsystem, double position) {
+    tes = trapezoidProfileSubsystem;
+    this.position = position;
+    this.velocity = 0.0;
+    addRequirements(tes);
+  }
+
+  @Override
+  public void initialize() {
+    limitSwitchStatus_UP = !tes.getLimitSwitch_UP().get();
+    limitSwitchStatus_DOWN = !tes.getLimitSwitch_DOWN().get();
+    goal = new TrapezoidProfile.State(position, velocity);
+    tes.setGoal(goal);
+  }
+
+  @Override
+  public void execute() {
+    currentPosition = tes.getEncoder().getDistance();
+    currentVelocity = tes.getEncoder().getRate();
+    tes.useState(new TrapezoidProfile.State(currentPosition, currentVelocity));
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    if (tes.getLimitSwitch_UP().get() == limitSwitchStatus_UP) {
+      new TrapezoidCommandSetGoal(tes, ConstantsElevator.ENCODER_FINAL).schedule();
+
+    } else if (tes.getLimitSwitch_DOWN().get() == limitSwitchStatus_DOWN){
+      new TrapezoidCommandSetGoal(tes, ConstantsElevator.ENCODER_INITIAL).schedule();
+      
+    } else {
+      tes.getElevatorMotorsGroup().set(0.0);
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    boolean stop = (currentPosition == goal.position && currentVelocity == goal.velocity) ||
+    (tes.getLimitSwitch_UP().get() == limitSwitchStatus_UP) || 
+    (tes.getLimitSwitch_DOWN().get() == limitSwitchStatus_DOWN);
+    return stop;
+  }
+}
---
+++ src/main/java/frc/robot/subsystems/TrapezoidElevatorSubsystem.java
@@ -0,0 +1,73 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.SpeedControllerGroup;
+import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.controller.ElevatorFeedforward;
+import edu.wpi.first.wpilibj.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj2.command.TrapezoidProfileSubsystem;
+import frc.robot.constants.ConstantsElevator;
+
+
+public class TrapezoidElevatorSubsystem extends TrapezoidProfileSubsystem {
+  
+  private ElevatorFeedforward elevatorFeedforward;
+  
+  private VictorSP elevatorMotor_1, elevatorMotor_2;
+  private SpeedControllerGroup elevatorMotorsGroup;
+
+  private Encoder encoderElevator;
+  private DigitalInput limitSwitchElevator_UP;
+  private DigitalInput limitSwitchElevator_DOWN;
+
+  public TrapezoidElevatorSubsystem(int ID_1, int ID_2, Encoder encoderElevator, 
+                                    DigitalInput limitSwitchElevator_UP, DigitalInput limitSwitchElevator_DOWN) {
+
+    super(new TrapezoidProfile.Constraints(ConstantsElevator.MAX_VELOCITY, ConstantsElevator.MAX_ACCELERATION), 
+                                           ConstantsElevator.INITIAL_POSITION);
+
+    this.elevatorMotor_1 = new VictorSP(ID_1);
+    this.elevatorMotor_2 = new VictorSP(ID_2);
+    this.encoderElevator = encoderElevator;
+    this.limitSwitchElevator_UP = limitSwitchElevator_UP;
+    this.limitSwitchElevator_DOWN = limitSwitchElevator_DOWN;
+    this.elevatorMotorsGroup = new SpeedControllerGroup(elevatorMotor_1, elevatorMotor_2);
+    this.elevatorFeedforward = new ElevatorFeedforward(ConstantsElevator.kS, 
+                                                       ConstantsElevator.kG,
+                                                       ConstantsElevator.kV, 
+                                                       ConstantsElevator.kA);
+    
+    this.encoderElevator.setDistancePerPulse(ConstantsElevator.DISTANCE_PER_PULSE);
+  }
+
+  public Encoder getEncoder() {
+    return encoderElevator;
+  }
+
+  public SpeedControllerGroup getElevatorMotorsGroup() {
+    return elevatorMotorsGroup;
+  }
+
+  public DigitalInput getLimitSwitch_UP() {
+    return limitSwitchElevator_UP;
+  }
+
+  public DigitalInput getLimitSwitch_DOWN() {
+    return limitSwitchElevator_DOWN;
+  }
+
+  @Override
+  public void useState(TrapezoidProfile.State setpointState) { 
+    double feedforward = elevatorFeedforward.calculate(setpointState.position, setpointState.velocity);
+    if(feedforward > 12) {
+      this.elevatorMotorsGroup.setVoltage(feedforward/12);
+    } else {
+    this.elevatorMotorsGroup.setVoltage(feedforward);
+    }
+  }
+}
